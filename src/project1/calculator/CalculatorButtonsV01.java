69913e94f9ebff78a9db8aee717c207356284038
Andres Malines <andres.malines@upr.edu>
2011-10-22 01:01:32 -0400

    Spaced (again)

--- src/project1/calculator/CalculatorButtonsV01.java
+++ src/project1/calculator/CalculatorButtonsV01.java
@@ -343,7 +343,7 @@ public class CalculatorButtonsV01 implements ActionListener, KeyListener {
 		else if (e.getSource() == buttons[12])
 		{
 			calcFunctions.setLastPressedButton("1/x");
-			if(txtField.getText().equals("inverse"))
+			if(txtField.getText().equals("0"))
 				txtField.setText("0");
 			else if(txtField.getText().length() != 0)
 				txtField.setText(calcFunctions.inverse(txtField.getText()));
